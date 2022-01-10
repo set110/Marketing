4e477959a33d4a766e8cc040821c8415886f6c0c
set110 <set110@ya.ru>
2022-01-10 21:19:05 +0300

    new 09.01.22

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function () {
-  
+
+   $('.slider-blog__inner').slick({
+      dots:true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
+   })
+
+
    var mixer = mixitup('.portfolio__content');
 
 });
